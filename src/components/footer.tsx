cfb2b83cf2a7b44e9685c12bb007d9f78cdcca0e
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-05-29 14:28:52 +0100

    Added responsiveness to the product page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-[#F4F3F1]">
-            <div className="px-12 pt-10 *:md:w-[22%] *:w-full md:gap-y-0 gap-y-12 flex md:flex-row flex-col justify-between mb-20">
+            <div className="lg:px-12 md:px-8 px-12 pt-10 *:md:w-[22%] *:w-full md:gap-y-0 gap-y-12 flex md:flex-row flex-col justify-between mb-20">
                 <section className="pb-4.25 space-y-5.75">
                     <h2 className="leading-7 text-[20px]">BETATRUS</h2>
                     <p className="font-manrope text-[#1A1C1A] text-[14px]">Defining the standard for the modern hair atelier through craftsmanship and editorial excellence.</p>
@@ -24,11 +24,13 @@ const Footer = () => {
                 </section>
                 <section className="space-y-6">
                     <h2 className="font-manrope font-semibold text-primary text-[14px] tracking-[1.2px]">NEWSLETTER</h2>
-                    <input type="email" name="email" id="email" placeholder="Email Address" className="border border-[#D4C3BE] px-4 py-2.5" />
-                    <button>Subscribe</button>
+                    <div className="flex flex-col items-start gap-5">
+                        <input type="email" name="email" id="email" placeholder="Email Address" className="border border-[#D4C3BE] px-4 py-2.5 w-full" />
+                        <button>Subscribe</button>
+                    </div>
                 </section>
             </div>
-            <div className="px-12 py-8 flex justify-between text-[16px] border-t border-t-[#D4C3BE]">
+            <div className="px-12 py-8 flex md:flex-row flex-col justify-between items-center gap-y-5 md:text-[16px] text-[14px] border-t border-t-[#D4C3BE]">
                 <p className="font-manrope text-[#1A1C1A] leading-4 tracking-[1.2px]">© 2026 BETATRUS. Crafted for the Modern Atelier.</p>
                 <div className="flex gap-5">
                     <p>Instagram</p>
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -8,20 +8,20 @@ const ProductDescription = () => {
     return (
         <div>
             <Header />
-            <main className="font-manrope p-10 text-[#504441]">
+            <main className="font-manrope py-10 lg:px-10 md:px-3 px-2 text-[#504441]">
                 <ul className="breadcrumnb flex items-center gap-2 text-[16px] text-[#504441] mb-12 leading-6">
                     <li>Home</li>
                     <li className="text-[12px]">&gt;</li>
                     <li>Bestseller</li>
                     <li className="text-[12px]">&gt;</li>
-                    <li className="text-black">Silk-Press Straight Frontal Wig</li>
+                    <li className="text-black md:text-[16px] text-[13px]">Silk-Press Straight Frontal Wig</li>
                 </ul>
-                <section className="flex justify-between mb-12">
-                    <div  className="w-[55%]">
-                        <div className="w-full h-200 overflow-hidden rounded-xl mb-6">
+                <section className="flex md:flex-row flex-col justify-between mb-12 gap-y-5">
+                    <div  className="md:w-[55%] w-full">
+                        <div className="w-full md:h-200 h-100 overflow-hidden rounded-xl mb-6">
                             <img src={wigImage} alt="" className="w-full h-full object-cover object-top" />
                         </div>
-                        <div className="*:w-[23%] *:h-38 gap-[2%] flex">
+                        <div className="*:md:w-[23%] *:w-[35%] w-full *:shrink-0 *:md:h-38 h-35 gap-[2%] flex md:overflow-hidden overflow-x-auto">
                             <div className="rounded-xl overflow-hidden border-2 border-black">
                                 <img src={wigImage} alt="" className="w-full h-full object-cover object-top" />
                             </div>
@@ -36,10 +36,10 @@ const ProductDescription = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-[40%]">
-                        <h2 className="font-noto text-[48px] font-bold leading-12 mb-3 text-black">Silk-Press Straight Frontal Wig</h2>
+                    <div className="md:w-[40%] w-full">
+                        <h2 className="font-noto lg:text-[48px] md:text-[38px] text-[30px] font-bold leading-12 mb-3 text-black">Silk-Press Straight Frontal Wig</h2>
                         <div className="flex items-center-safe gap-5 mb-10">
-                            <p className="text-[24px] font-medium text-primary">$495.00</p>
+                            <p className="md:text-[24px] text-[20px] font-medium text-primary">$495.00</p>
                             <div className="h-4 bg-[#504441] w-px" />
                             <div className="text-primary flex *:w-6 *:h-4.75">
                                 <Star fill="blue" />
@@ -48,7 +48,7 @@ const ProductDescription = () => {
                                 <Star fill="blue" />
                                 <Star fill="blue" />
                             </div>
-                            <p className="text-[16px] leading-5">(24 Reviews)</p>
+                            <p className="text-[16px] leading-5 lg:block md:hidden ">(24 Reviews)</p>
                         </div>
                         <div className="flex justify-between mb-4">
                             <p className="font-medium text-[16px]">LENGTH</p>
@@ -87,23 +87,23 @@ const ProductDescription = () => {
                         </button>
                         <div className="border-t border-t-[#d4c3be63] pt-10">
                             <h3 className="text-[20px] font-noto italic mb-10 text-black">Description</h3>
-                            <p className="text-[18px] leading-6.5 text-black">
+                            <p className="md:text-[18px] text-[16px] leading-6.5 text-black">
                                 Our signature Silk-Press Straight Frontal Wig features 100% ethically sourced virgin hair, double-drawn for consistent density from root to tip. The 13x6 HD lace frontal ensures a seamless, melt-into-skin finish that allows for multi-directional parting and effortless versatility.
                             </p>
                         </div>
                     </div>
                 </section>
-                <section className="border-t border-t-[#1c1b1b18] pt-6 pb-12 mb-12">
-                    <ul className="flex gap-12 *:pb-3 *:text-[16px] mb-6">
+                <section className="border-t border-t-[#1c1b1b18] pt-6 pb-12 md:mb-12 mb-6">
+                    <ul className="flex gap-12 *:pb-3 *:md:text-[16px] *:text-[12px] mb-6">
                         <li className="border-b-2 border-b-[#0000FF]">PRODUCT SPECS</li>
                         <li>SHIPPING & RETURNS</li>
                         <li>VERIFIED REVIEWS (12)</li>
                     </ul>
-                    <div className="flex gap-[4%] text-black mb-12">
-                        <section className="w-[48%]">
-                            <h2 className="font-medium text-[24px] leading-7.5 mb-6 font-bodoni">Material & Construction</h2>
+                    <div className="flex md:flex-row flex-col gap-[4%] gap-y-7 text-black mb-12">
+                        <section className="md:w-[48%] w-full">
+                            <h2 className="font-medium md:text-[24px] text-[20px] leading-7.5 mb-6 font-bodoni">Material & Construction</h2>
                             <div className="w-full">
-                                <table className="w-full *:flex *:justify-between text-[16px] space-y-4 leading-6 font-medium">
+                                <table className="w-full *:flex *:justify-between md:text-[16px] text-[14px] space-y-4 leading-6 font-medium">
                                     <tr className="border-b border-b-[#1c1b1b18] pb-2">
                                         <td>Hair Type</td>
                                         <td>100% Unprocessed Cambodian Human Hair</td>
@@ -123,9 +123,9 @@ const ProductDescription = () => {
                                 </table>
                             </div>
                         </section>
-                        <section className="w-[48%]">
-                            <h2 className="font-medium text-[24px] leading-7.5 mb-6 font-bodoni">Styling & Care</h2>
-                            <p className="text-[16px] leading-6 font-medium">
+                        <section className="md:w-[48%] w-full">
+                            <h2 className="font-medium md:text-[24px] text-[20px] leading-7.5 mb-6 font-bodoni">Styling & Care</h2>
+                            <p className="md:text-[16px] text-[14px] leading-6 font-medium">
                                 This luxury frontal can be dyed up to 613 platinum blonde without losing its silkiness. The HD lace is pre-plucked with natural baby hairs to ensure an invisible transition. We recommend professional installation for the most natural look. Use sulfate-free shampoos and cool water for longevity.
                             </p>
                         </section>
@@ -133,10 +133,10 @@ const ProductDescription = () => {
                 </section>
                 <section>
                     <div className="flex justify-between mb-6">
-                        <h2 className="text-[32px] font-medium leading-9.5 font-bodoni">Verified Purchases</h2>
+                        <h2 className="md:text-[32px] text-[20px] font-medium leading-9.5 font-bodoni">Verified Purchases</h2>
                         <p className="text-[16px] text-primary underline">Write a Review</p>
                     </div>
-                    <div className="flex *:w-[48%] gap-[4%]">
+                    <div className="flex md:flex-row flex-col *:md:w-[48%] w-full gap-[4%]">
                         <section className="p-8 bg-[#F0EDED] border space-y-4 border-[#1c1b1b18] text-black">
                             <div className="flex justify-between">
                                 <div className="flex gap-2">
