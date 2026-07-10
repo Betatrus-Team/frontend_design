83b99c141229bda833a775888cb19a7b16d1f3f9
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-07-10 16:57:36 +0100

    Completed the checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,15 +1,16 @@
-import { ArrowLeft, CreditCard, Grid3X3Icon, Landmark, LockKeyhole, QrCode, RefreshCcwDotIcon, ShieldCheck } from "lucide-react";
+import { ArrowLeft, Award, CreditCard, Grid3X3Icon, Landmark, LockKeyhole, QrCode, RefreshCcwDotIcon, ShieldCheck } from "lucide-react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Button from "../components/button";
+import myWig from "../assets/images/wig.png";
 
 const Checkout = () => {
     return (
         <div>
             <Header />
             <main className="w-full px-10 py-20 font-manrope">
-                <div className="flex text-[#1a1c1a]">
-                    <section className="space-y-12 w-[60%]">
+                <div className="flex lg:flex-row flex-col gap-16 text-[#1a1c1a]">
+                    <section className="space-y-12 lg:w-[60%] w-full">
                         <div>
                             <h1 className="text-[48px] leading-12 tracking-[-1.2px] font-noto font-bold">Select Payment Method</h1>
                             <p className="text-[16px] leading-6 text-[#504441]">Choose your preferred way to complete your purchase at our atelier.</p>
@@ -91,6 +92,42 @@ const Checkout = () => {
                             <p><ShieldCheck className="w-[13.33px] h-[17.5px]" /> SECURE SSL</p>
                             <p><LockKeyhole className="w-[13.33px] h-[17.5px]" /> ENCRYPTED</p>
                             <p><RefreshCcwDotIcon className="w-[13.33px] h-[17.5px]" /> PCI-DSS</p>
+                        </div>
+                    </section>
+                    <section className="lg:w-">
+                        <div className="bg-[#F4F3F1] space-y-8 p-10 rounded-2xl">
+                            <h2 className="font-bold text-6 font-noto leading-8">Order Summary</h2>
+                            <div className="flex items-center gap-3 pb-8 border-b border-b-[#d4c3be5d]">
+                                <div className="w-23 h-20 object-fit">
+                                    <img src={myWig} className="w-full h-full" />
+                                </div>
+                                <div>
+                                    <p className="font-noto text-[18px] leading-[22.5px]">Silk-press Straight Frontal Wig</p>
+                                    <p className="font-manrope text-[14px] leading-4 text-[#504441]">24 inches / Natural Black</p>
+                                    <p className="font-manrope text-[16px] leading-6 font-bold">₦420,000</p>
+                                </div>
+                            </div>
+                            <div className="space-y-3 pb-8 border-b border-b-[#d4c3be5d]">
+                                <div className="flex justify-between">
+                                    <p className="text-[16px] font-manrope leading-6">Subtotal</p>
+                                    <p className="text-[16px] font-medium font-manrope leading-6">₦420,000</p>
+                                </div>
+                                <div className="flex justify-between">
+                                    <p className="text-[16px] font-manrope leading-6">Shipping</p>
+                                    <p className="text-[16px] font-medium font-manrope leading-6">₦2,000</p>
+                                </div>
+                            </div>
+                            <div className="pt-6">
+                                <p className="font-manrope text-[14px] text-[#504441] leading-5 font-bold tracking-[1.4px]">TOTAL AMOUNT</p>
+                                <div className="flex justify-between">
+                                    <p className="font-noto text-[30px] leading-7.5 font-bold">₦422,500</p>
+                                    <p className="text-[#504441] text-[12px] leading-4 font-manrope">VAT included</p>
+                                </div>
+                            </div>
+                            <div className="bg-[#50444115] text-[#504441] font-manrope text-[12px] leading-4.75 flex gap-3 p-4 rounded-xl">
+                                <Award className="text-primary" />
+                                <p>Every purchase at Trustora is backed by our authenticity guarantee and premium delivery service.</p>
+                            </div>
                         </div>
                     </section>
                 </div>
